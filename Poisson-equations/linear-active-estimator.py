2094280e1fdcb0c88aa0985abf0ba0ba42c0217e
Unique Subedi <38981612+unique-subedi@users.noreply.github.com>
2024-12-04 13:53:03 -0500

    Update linear-active-estimator.py

--- Poisson-equations/linear-active-estimator.py
+++ Poisson-equations/linear-active-estimator.py
@@ -1,12 +1,34 @@
 import sys
 sys.path.append("data-generation")
 import numpy as np
-from Laplacian import Laplacian
-from GRF import GRF
-from cosines import gen_cosines
 from scipy.sparse.linalg import spsolve
 from scipy.fftpack import dctn
 from scipy.fftpack import idctn
+
+
+def Laplacian(N):
+    N2 = (N-1) * (N-1)
+    h=1/N
+  
+    # Define diagonals for the discrete Laplacian operator
+    main_diag = -4 * np.ones(N2)
+    side_diag = np.ones(N2 - 1)
+    side_diag[np.arange(1, N-1) * (N-1) - 1] = 0  # Adjust for block boundaries
+    up_down_diag = np.ones(N2 - (N-1))
+
+    # Create sparse matrix with specified diagonals
+    L = (1/h**2)*diags([main_diag, side_diag, side_diag, up_down_diag, up_down_diag],
+              [0, -1, 1, -(N-1), (N-1)], format="csc")
+
+    return L
+
+
+
+N = 64
+L = Laplacian(N)
+alpha = 50**2
+beta = 1
+gamma=2
 
 
 
