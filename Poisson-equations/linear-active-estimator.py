a8d5fbe4b5de3a10e23c4eddb13a5c365c56a639
Unique Subedi <38981612+unique-subedi@users.noreply.github.com>
2024-12-04 14:11:04 -0500

    Update linear-active-estimator.py

--- Poisson-equations/linear-active-estimator.py
+++ Poisson-equations/linear-active-estimator.py
@@ -21,6 +21,17 @@ def Laplacian(N):
 
     return L
 
+def gen_cosines(N, k1, k2):
+    # Initialize an N x N array with zeros in the frequency domain
+    init_array = np.zeros((N, N))
+    # Place "spike" at (k1, k2))
+    init_array[k1,k2] = 1
+
+    # Perform the inverse DCT to obtain the spatial cosine pattern
+    cosines = idctn(init_array, type=2)
+
+    return 1/2*cosines
+
 
 
 N = 64
