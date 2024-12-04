9e9d4978f08edebe9120457ed3be5d0714cbdb1e
Unique Subedi <38981612+unique-subedi@users.noreply.github.com>
2024-12-04 12:52:32 -0500

    Update cosines.py

--- cosines.py
+++ cosines.py
@@ -4,7 +4,7 @@ from scipy.fftpack import idctn
 def gen_cosines(N, k1, k2):
     # Initialize an N x N array with zeros in the frequency domain
     init_array = np.zeros((N, N))
-    # Set the appropriate frequency component to 1 (place "spike" at (k1, k2))
+    # Place "spike" at (k1, k2))
     init_array[k1,k2] = 1
 
     # Perform the inverse DCT to obtain the spatial cosine pattern
